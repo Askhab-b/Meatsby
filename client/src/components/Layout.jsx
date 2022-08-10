75bd1a0c268f299f7877fce0ae21506242554d89
saynz <hzrv.ms@gmail.com>
2022-08-10 20:17:45 +0300

    Организовал возможность добавления анимаций

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,12 +1,11 @@
-// import { useEffect } from 'react'
-// import Aos from "aos";
-// import "aos/dist/aos.css";
+import { useEffect } from 'react'
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-const Layout = ({ children }) => {
-  
-  // useEffect(() => {
-  //   Aos.init({ duration: 1000 });
-  // }, [])
+const Layout = ({ children }) => {  
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, [])
 
   return (
     <>
