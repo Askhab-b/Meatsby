1d22796a29e6ce5df13393ba65159420d1642648
saynz <hzrv.ms@gmail.com>
2022-08-10 20:16:29 +0300

    Сделал роуты современнее

--- server/routes/ingredient.route.js
+++ server/routes/ingredient.route.js
@@ -2,10 +2,10 @@ const { Router } = require('express');
 const { ingredientController } = require('../controllers/ingredient.controller');
 const router = Router();
 
-router.post('/ingredient', ingredientController.postIngredient);
-router.get('/ingredient', ingredientController.getAllIngredients);
-router.get('/ingredient/:id', ingredientController.getIngredientById);
-router.delete('/ingredient/:id', ingredientController.deleteIngredientById);
-router.patch('/ingredient/:id', ingredientController.patchIngredient);
+router.post('/ingredient', ingredientController.postIngredient)
+      .get('/ingredient', ingredientController.getAllIngredients)
+      .get('/ingredient/:id', ingredientController.getIngredientById)
+      .delete('/ingredient/:id', ingredientController.deleteIngredientById)
+      .patch('/ingredient/:id', ingredientController.patchIngredient)
 
 module.exports = router;
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -3,10 +3,10 @@ const { productController } = require('../controllers/product.controllers');
 const router = Router();
 const fileMiddleware = require('../middleware/file');
 
-router.post('/product', fileMiddleware.single('img'), productController.postProduct);
-router.get('/product', productController.getAllProducts);
-router.get('/product/:id', productController.getProductById);
-router.delete('/product/:id', productController.deleteProductById);
-router.patch('/product/:id', productController.patchProduct);
+router.post('/product', fileMiddleware.single('img'), productController.postProduct)
+      .get('/product', productController.getAllProducts)
+      .get('/product/:id', productController.getProductById)
+      .delete('/product/:id', productController.deleteProductById)
+      .patch('/product/:id', productController.patchProduct)
 
 module.exports = router;
