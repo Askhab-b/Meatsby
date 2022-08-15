6abea87443b98b2562c1c1cadc799b8b6e39c317
H Mans <hzrv.ms@gmail.com>
2022-08-15 11:26:48 +0300

    Дописал и добавил в Store пару запросов

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,15 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './shopping-cart/authSlice';
-import cartReducer from './shopping-cart/cartSlice';
-import cartUiSlice from './shopping-cart/cartUiSlice';
-import productSlice from './shopping-cart/productSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./shopping-cart/authSlice";
+import cartSlice from "./shopping-cart/cartSlice";
+import cartUiSlice from "./shopping-cart/cartUiSlice";
+import productSlice from "./shopping-cart/productSlice";
 
 const store = configureStore({
   reducer: {
-    cart: cartReducer,
-    cartUi: cartUiSlice,
-    auth: authReducer,
-    product: productSlice,
+    cart: cartSlice.reducer,
+    cartUi: cartUiSlice.reducer,
+    auth: authSlice,
+    product: productSlice
   },
 });
 
